3134e77865d919c7e0c071253e67d6db935af0bc
Marvin <marvinlimson2007@gmail.com>
2019-03-18 14:18:30 +0000

    Played with JSX properties and rules

--- src/index.js
+++ src/index.js
@@ -5,9 +5,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // 2. Create React component
+
+function getButtonText(){
+    return "Confirm";
+}
+//You can reference a function in JSX
 const App = () => {
-    return <div>Hi there!</div>;
+    const buttonText = "Click Me!";//Or a variable
+    //However you can't put in Javascript Objects like buttonText = {text: "Click Me"}
+    //Unless you put in buttonText.text instead of just buttonText
+    //Also check console for other incompatible properties
+    return (
+        <div>
+            <label className="label" htmlFor="name">Enter name:</label>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>{getButtonText()}</button>
+        </div> 
+    );
 };
+
 
 // 3. Take React component and display on screen
 ReactDOM.render(
